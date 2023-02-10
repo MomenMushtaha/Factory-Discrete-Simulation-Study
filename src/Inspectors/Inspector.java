5bc193eebca3c57e672b46fd424a04e315652462
Momen Mushtaha <mushmome1@gmail.com>
2023-02-10 16:50:15 -0500

    Modified inspector and Workstation

--- src/Inspectors/Inspector.java
+++ src/Inspectors/Inspector.java
@@ -1,22 +1,32 @@
 package Inspectors;
-
-
 import java.util.Random;
-import Buffers_Product.*;
-
-public class inspector2 {
+//Created by Mohamed Abdalla
+//Modified by Momin Mushtaha
+public class Inspector {
     /*
      * Calculate average inspection time.
      * Randomly choose betwwen c2 or c3
      * Send to Buffer class
-    */
-
+     */
+    private double averageTime;
     private double averageTime22;
     private double averageTime23;
-
     private boolean buffer;
-    
-    public inspector2(double averageTime22, double averageTime23){
+    private Integer Id;
+
+
+    //constructor for inspector1
+    public Inspector(Integer Id, double averageTime){
+        this.Id = Id;
+        this.averageTime = averageTime;
+        getAverageTime();
+        //buffer statement add here
+    }
+
+
+    //constructor for inspector2
+    public Inspector(Integer Id, double averageTime22, double averageTime23){
+        this.Id = Id;
         this.averageTime22 = averageTime22;
         this.averageTime23 = averageTime23;
         getAverageTime22();
@@ -24,10 +34,10 @@ public class inspector2 {
         buffer = c2Orc3();
     }
 
+
     private boolean c2Orc3() {
         Random rand = new Random();
         return rand.nextBoolean();
-        
     }
 
     public double getAverageTime22() {
@@ -38,5 +48,10 @@ public class inspector2 {
     public double getAverageTime23() {
         System.out.println("ins2:buf3 avg = " + averageTime23 + "\n");
         return averageTime23;
+    }
+
+    public  double getAverageTime(){
+        System.out.println("ins1:buf1 avg = " + averageTime + "\n");
+        return averageTime;
     }
 }
